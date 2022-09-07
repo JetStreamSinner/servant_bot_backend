572b4e340cec9ed4a56f8ce735de29ce9c598395
Jetstream <bigalex934@gmail.com>
2022-09-07 23:27:05 +0300

    Receiving services repository is wrapped by fastapi dependency injection system

--- app/api/dependencies/services.py
+++ app/api/dependencies/services.py
@@ -1,8 +1,10 @@
 from app.core.config import get_app_settings
 from app.database.repositories.services import ServicesRepository
 
-app_settings = get_app_settings()
-source_uri = app_settings.service_source_uri
-source_type = app_settings.service_source_type
 
-services_repository: ServicesRepository = ServicesRepository(source_uri=source_uri, source_type=source_type)
+def get_services_repository() -> ServicesRepository:
+    app_settings = get_app_settings()
+    source_uri = app_settings.service_source_uri
+    source_type = app_settings.service_source_type
+    services_repository: ServicesRepository = ServicesRepository(source_uri=source_uri, source_type=source_type)
+    return services_repository
--- app/api/routes/services.py
+++ app/api/routes/services.py
@@ -1,22 +1,23 @@
 from typing import List
 
-from fastapi import APIRouter, HTTPException, Request
+from fastapi import APIRouter, HTTPException, Request, Depends
 
-from app.models.schemas.service import ServiceOuter, ServiceDeclaration
-
-from app.api.dependencies.services import services_repository
+from app.api.dependencies.services import get_services_repository
 from app.api.dependencies.task import push_task_to_service
+from app.database.repositories.services import ServicesRepository
+from app.models.schemas.service import ServiceOuter, ServiceDeclaration
 
 services_router = APIRouter()
 
 
 @services_router.get(path="/services_list", response_model=List[ServiceDeclaration])
-async def services_list_handler():
+async def services_list_handler(services_repository: ServicesRepository = Depends(dependency=get_services_repository)):
     return services_repository.get_services_list()
 
 
 @services_router.get(path="/service_info/{service_id}", response_model=ServiceOuter)
-async def services_list_handler(service_id: int):
+async def services_list_handler(service_id: int,
+                                services_repository: ServicesRepository = Depends(dependency=get_services_repository)):
     try:
         return services_repository.get_service_info(service_id=service_id)
     except RuntimeError:
@@ -24,7 +25,8 @@ async def services_list_handler(service_id: int):
 
 
 @services_router.post(path="/add_task/{service_id}")
-async def services_list_handler(service_id: int, request: Request):
+async def services_list_handler(service_id: int, request: Request,
+                                services_repository: ServicesRepository = Depends(dependency=get_services_repository)):
     try:
         service_info = services_repository.get_service_info(service_id=service_id)
     except RuntimeError:
