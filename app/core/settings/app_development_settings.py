f1b94c9441c6c2ef031a4b1172194eded1fe1170
Jetstream <bigalex934@gmail.com>
2022-09-07 23:47:44 +0300

    Little hack for working with sqlite database

--- app/core/settings/app_development_settings.py
+++ app/core/settings/app_development_settings.py
@@ -3,5 +3,6 @@ from app.core.settings.app import DatasourceTypes
 
 
 class AppDevelopmentSettings(AppSettings):
-    service_source_uri = "sqlite:///app/resources/develop_database.db"
+    # TODO Same as in test settings configure
+    service_source_uri = "sqlite:///app/resources/develop_database.db?check_same_thread=False"
     service_source_type = DatasourceTypes.database
--- app/core/settings/app_test_settings.py
+++ app/core/settings/app_test_settings.py
@@ -3,5 +3,9 @@ from app.core.settings.app import DatasourceTypes
 
 
 class AppTestSettings(AppSettings):
-    service_source_uri = "sqlite:///tests/resources/test_database.db"
+    # TODO Using check_same_thread is just a hack.
+    # TODO Need to rework system with repositories and data providers
+    # TODO because repository object and accordingly database object
+    # TODO recreated when call any endpoint happened
+    service_source_uri = "sqlite:///tests/resources/test_database.db?check_same_thread=False"
     service_source_type = DatasourceTypes.database
